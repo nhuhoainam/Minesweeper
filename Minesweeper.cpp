982a57c63aae5491a97aa83898964817cc231ca7
HoaiNam <hoainamnguyennhu7@gmail.com>
2021-12-01 09:33:28 +0700

    add console functions

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
-#include <unistd.h>
+#include <windows.h>
 
 using namespace std;
 
 #include "Init.h"
 #include "Command.h"
 #include "Display.h"
+#include "consoletext.h"
 
 void GamePlay()
 {
     int Board[100][100], isMine[100][100], row, col, Mines, isFlag[100][100];
     bool isFlip[100][100];
+    memset(Board, 0, sizeof Board);
+    memset(isMine, 0, sizeof isMine);
+    memset(isFlag, 0, sizeof isFlag);
+    memset(isFlip, 0, sizeof isFlip);
     Init(Board, row, col, Mines, isFlip, isMine, isFlag);
     int GameOver = 0, ValidCells = row * col - Mines;
     while (!GameOver)
@@ -22,7 +27,7 @@ void GamePlay()
             DisplayBoard(Board, row, col, Mines, isFlip, isMine, isFlag);
             if (GameOver == 1) cout << "\nU lose\n";
             else cout << "\nU win\n";
-            sleep(3);
+            Sleep(3);
         }
     }
 }
@@ -30,6 +35,9 @@ void GamePlay()
 int main()
 {
     int c;
+    DisableResizeWindow();
+    SetWindowSize(60, 15);
+    SetScreenBufferSize(60, 15);
     while (1)
         {
             system("cls");
